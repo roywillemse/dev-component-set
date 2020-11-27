5c67d6698aa442ee951122049ff5737bf73ea738
Roy Willemse <roy.willemse@bettyblocks.com>
2020-11-27 09:13:13 +0100

    initial commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import * as Core from '@material-ui/core';
-import * as Lab from '@material-ui/lab';
-import * as Pickers from '@material-ui/pickers';
-import * as Styles from '@material-ui/styles';
-import DateFnsUtils from '@date-io/date-fns';
-import Icons from './icons';
-
-export default { Core, Icons, Lab, Pickers, Styles, DateFnsUtils };
--- src/interactions/setCurrentRecord.ts
+++
@@ -1,9 +0,0 @@
-function setCurrentRecord({
-  event,
-  argument,
-}: {
-  event: Event;
-  argument: number;
-}): number {
-  return argument;
-}
